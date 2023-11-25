6a39d088686fde0e6f7f27f18549d6e5a5be86c5
Mateusz Juszczak <mateusz.juszczak@protonmail.com>
2023-11-25 15:30:06 +0100

    Created basic web gui and implemented some project requirements regarding its behaviour.

--- skrypt.py
+++ skrypt.py
@@ -1,4 +1,10 @@
 import math
+import plotly.graph_objects as go
+import dash 
+from dash import html
+import dash_core_components as dcc
+import dash_bootstrap_components as dbc
+
 
 air_density = 1.293
 frontal_area = 1.89
@@ -61,5 +67,158 @@ def wind_force(v, wind_v):
 
 
 
-for _ in range(t // ts):
+#for _ in range(t // ts):
+
+def function1(x, p1):
+    return p1 * x
+
+def function2(x, p1, p2):
+    return p1 * x + p2
+
+def function3(x, p1, p2, p3):
+    return (p1 * x + p2) % p3
+p1 = 3
+p2 = 20
+p3 = 40
+
+#parametry sliderow
+min1 = 1
+max1 = 10
+value1 = (min1 + max1) / 2
+min2 = 1
+max2 = 10
+value2 = (min1 + max1) / 2
+min3 = 1
+max3 = 10
+value3 = (min1 + max1) / 2
+
+x = [i for i in range(20)]
+
+app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
+
+app.layout = dbc.Container(
+    [
+        html.H1("TEMPOMAT"),
+        dbc.Row(
+            [
+                dbc.Col(
+                    html.Div(
+                        [
+                            html.H2("parametry pojazdu"),
+                            html.H3("wybór modelu:"),
+                            dcc.Dropdown(
+                                id='dropdown_menu',
+                                options=
+                                [
+                                    {'label':'autko1', 'value' : 'opt1'},
+                                    {'label':'autko2', 'value' : 'opt2'},
+                                    {'label':'custom', 'value' : 'opt3'}
+                                ],
+                                value='opt3'
+                            ),
+                            html.H3("parametr1:"),
+                            dcc.Slider(
+                                id='slider1',
+                                min=min1,
+                                max=max1,
+                                step=1,
+                                value=value1,
+                                marks={i: str(i) for i in range(min1, max1+1)}
+                            ),
+                            html.H3("parametr2:"),
+                            dcc.Slider(
+                                id='slider2',
+                                min=min2,
+                                max=max2,
+                                step=1,
+                                value=value2,
+                                marks={i: str(i) for i in range(min2, max2+1)}
+                            ),
+                            html.H3("parametr3:"),
+                            dcc.Slider(
+                                id='slider3',
+                                min=min3,
+                                max=max3,
+                                step=1,
+                                value=value3,
+                                marks={i: str(i) for i in range(min3, max3+1)}
+                            ),
+                            html.H2("parametry symulacji"),
+                            html.Button("aktualizuj", id='button')
+                        ]
+                    ),
+                    width=3
+                ),
+                dbc.Col(
+                    html.Div(
+                        [
+                            dcc.Graph(id='wykres1'),
+                            dcc.Graph(id='wykres2'),
+                            dcc.Graph(id='wykres3')
+                        ]
+                    ),
+                    width=9
+                )
+            ]
+        )
+    ],
+    fluid=True, 
+    style={'height': '100vh'}
+)
+
+@app.callback(
+    [
+        dash.dependencies.Output('wykres1', 'figure'),
+        dash.dependencies.Output('wykres2', 'figure'),
+        dash.dependencies.Output('wykres3', 'figure'),
+        dash.dependencies.Output('slider1', 'value'),
+        dash.dependencies.Output('slider2', 'value'),
+        dash.dependencies.Output('slider3', 'value'),
+    ],
+    [
+        dash.dependencies.Input('slider1', 'value'),
+        dash.dependencies.Input('slider2', 'value'),
+        dash.dependencies.Input('slider3', 'value'),
+        dash.dependencies.Input('dropdown_menu', 'value')
+    ]
+)
+
+def update_graphs(p1, p2, p3, selected_model):
+    if selected_model == 'opt1':
+        p1, p2, p3 = 1, 2, 3
+    elif selected_model == 'opt2':
+        p1, p2, p3 = 2, 4, 6
     
+    y1 = [function1(x,p1) for x in x]
+    y2 = [function2(x,p1,p2) for x in x]
+    y3 = [function3(x,p1,p2,p3) for x in x]
+
+    trace1 = go.Scatter(x=x, y=y1, mode='lines', name='wykres 1')
+    trace2 = go.Scatter(x=x, y=y2, mode='lines', name='wykres 2')
+    trace3 = go.Scatter(x=x, y=y3, mode='lines', name='wykres 3')
+    
+    layout1 = go.Layout(
+        title='wykres 1', 
+        xaxis=dict(title='x'), 
+        yaxis=dict(title='y')
+    )
+    layout2 = go.Layout(
+        title='wykres 2', 
+        xaxis=dict(title='x'), 
+        yaxis=dict(title='y')
+    )
+    layout3 = go.Layout(
+        title='wykres 3', 
+        xaxis=dict(title='x'), 
+        yaxis=dict(title='y')
+    )
+
+    fig1 = go.Figure(data=[trace1], layout=layout1)
+    fig2 = go.Figure(data=[trace2], layout=layout2)
+    fig3 = go.Figure(data=[trace3], layout=layout3)
+
+    return fig1, fig2, fig3, p1, p2, p3
+
+
+if __name__ == '__main__':
+    app.run_server(port = 4050)
